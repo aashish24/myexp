41104cda9261e3f115c077505cce6b25f478cf7b
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-06-22 03:45:46 +0000

    - Updated implementation w.r.t to c++ stub code.

--- Indra/tests/Tweek/cpp/MyApp.h
+++ Indra/tests/Tweek/cpp/MyApp.h
@@ -1,34 +1,26 @@
-//
-//:
-//:
 
-#ifndef _MY_APP_H_
-#define _MY_APP_H_
+#ifndef __MY_APP_H__
+#define __MY_APP_H__
 
-//: VJApp include.
-#include <OsgVJApp.h>
+#include "Veda/Core/OsgBase.h"
 
-//: Users's application class.
-class MyApp : public OsgVJApp 
+class MyApp : public Veda::Core::OsgBase 
 {
 	public:
 
 		MyApp( vrj::Kernel* kern, int& argc, char** argv );
-		~MyApp(){;}		
+
+    ~MyApp(){}
 
 		void appInit();
 		void appBufferPreDraw();		
-		//void appPostOsgDraw();
 		void appSceneInit();
 		void appLatePreFrame();
-		void takeAction(std::string command_identifier, 
-						std::string entity, 
-						std::string state_attribute, 
-						std::string modifier_string, 
-						bool modifier_bool, 
-						long modifier_long);
-		void loadGroupedData(osg::Group* groupNode, const std::string& fileName);
+		
+    void takeAction( const std::string& key, const std::string& value );
 
+		void loadGroupedData(osg::Group* groupNode, const std::string& fileName);
 };
-#endif //: _MY_APP_H_
+
+#endif // __MY_APP_H__
 
