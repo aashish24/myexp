2799de30742501349314ed23588132470f9b0878
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-06-26 05:13:24 +0000

    - Example works now with the textures and with given parameters.

--- Indra/modules/OsgTools/examples/OsgDashboard/OsgDashboardApp.cpp
+++ Indra/modules/OsgTools/examples/OsgDashboard/OsgDashboardApp.cpp
@@ -12,6 +12,7 @@ int main( int argc, char **argv )
     osg::ArgumentParser arguments( &argc,argv );
 
     osg::ref_ptr< osg::Group > scene( new osg::Group() );
+    osg::ref_ptr< OsgTools::Geom::OsgDashboard > dBoard( new OsgTools::Geom::OsgDashboard( 500.0, 500.0, osg::Vec3f( -10.0, 0.0, 0.0 ) ) );
 
     osg::ref_ptr< osg::Node > loadedModel = osgDB::readNodeFiles( arguments );    
     
@@ -21,9 +22,23 @@ int main( int argc, char **argv )
     }
 
     osgViewer::Viewer viewer;
-    
-    //scene->addChild( loadedModel.get() );
-    scene->addChild( new OsgTools::Geom::OsgDashboard( 500.0, 500.0, osg::Vec3f( -10.0, 0.0, 0.0 ) ) );
+        
+    // Load a sample texture. 
+    osg::ref_ptr< osg::Image > image = osgDB::readImageFile( "osg64.png" );
+    if( image.get() )
+    {
+      osg::ref_ptr< osg::Texture2D > texture = new osg::Texture2D( image.get() );
+
+      if( texture.valid() )
+      {
+        // Turn lighting calculation off on dashboard.
+        dBoard->getOrCreateStateSet()->setMode( GL_LIGHTING, osg::StateAttribute::OFF );
+        dBoard->setTexture( texture.get(), OsgTools::Geom::OsgDashboard::ALL );
+      }
+    }
+
+    scene->addChild( loadedModel.get() );        
+    scene->addChild( dBoard.get() );
     viewer.setSceneData( scene.get() );    
     return viewer.run();
 }
