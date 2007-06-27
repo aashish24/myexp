934e096f6dc122683642aaa65976cc5f9959ec75
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-06-27 22:58:24 +0000

    - Started adding cmake modules

---
+++ Indra/cmake/FindOSG.cmake
@@ -0,0 +1,20 @@
+
+
+#------Find osg Library----------------
+
+FIND_LIBRARY( OSG_LIB osg ${OSG_LIB_DIR} )
+
+
+#------Find osgDB Library--------------
+
+FIND_LIBRARY( OSGDB_LIB osgDB ${OSG_LIB_DIR} )
+
+
+#------Find osgUtil Library------------
+
+FIND_LIBRARY( OSGUTIL_LIB osgUtil ${OSG_LIB_DIR} )
+
+
+#------Find osgFX Library--------------
+
+FIND_LIBRARY( OSGFX_LIB osgFx ${OSG_LIB_DIR} )
