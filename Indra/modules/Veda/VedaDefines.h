cec1777e65b327fa15cd365a038b987763050df6
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-07-11 04:28:50 +0000

    - Deleting revision 128 for these files and adding revision 127 as 128 was not supposed to be in the repository

---
+++ Indra/modules/Veda/VedaDefines.h
@@ -0,0 +1,19 @@
+
+#ifndef __VEDA_CORE_VEDADEFINES_H__
+#define __VEDA_CORE_VEDADEFINES_H__
+
+//@Todo: Find a better way to determine vrjuggler version. 
+
+// If we are using OpenSceneGraph
+#define HAVE_OSG                1
+
+// Define vrjuggler major and minor version as the API has changes just a little bit 
+#define VRJUGGLER_MAJOR_VERSION 2
+
+#define VRJUGGLER_MINOR_VERSION 2
+
+// If we using KeyboardMouse for navigation. 
+#define HAVE_KEYBOARDMOUSE      0
+
+#endif // __VEDA_CORE_VEDADEFINES_H__
+
