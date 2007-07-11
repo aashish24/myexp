d49a8bc143a499f6827fdef26f9d47b672ad5612
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-07-11 07:20:10 +0000

    - Started adding Animation callbacks

---
+++ Indra/modules/OsgTools/Anime/OsgUniformCallback.cpp
@@ -0,0 +1,35 @@
+ 
+#include "OsgTools/Anime/OsgUniformCallback.h"
+
+namespace OsgTools
+{
+  namespace Anime
+  {
+    void OsgUniformCallback::operator() ( osg::Uniform* uniform, osg::NodeVisitor* nv )
+    {
+        if( mEnabled )
+        {   
+            static double counter = 0.0;
+            
+            switch( mMode )
+            {
+                case COUNTER: 
+                {
+                  if( counter >= 1.0 )
+                  {
+                    uniform->set( osg::Vec3( 1.0, 1.0, 1.0 ) );
+                  }
+                  else
+                  {
+                    uniform->set( osg::Vec3( counter, counter, counter ) );
+                    counter += 0.005;                                         
+                  }
+
+                  break;  
+                }
+                
+            }
+        }
+    }
+  }
+}
---
+++ Indra/modules/OsgTools/Anime/OsgUniformCallback.h
@@ -0,0 +1,40 @@
+
+#include "OsgTools/Core/Export.h"
+
+#include "osg/Uniform"
+
+namespace OsgTools
+{
+  namespace Anime
+  {
+    class OSGTOOLS_EXPORT OsgUniformCallback: public osg::Uniform::Callback
+    {
+
+        public:
+
+            enum Mode
+            {
+                COUNTER = 0,
+                TIME
+            };
+        
+            OsgUniformCallback( Mode op );            
+
+            virtual void operator() ( osg::Uniform* uniform, osg::NodeVisitor* nv );
+
+        private:
+          
+          bool                      mEnabled;
+
+          Mode                      mMode;
+
+          long double               mMin;
+          long double               mMax;
+          long double               mDelta;
+
+          bool                      mConsiderFPS;
+          long double               mDuration;
+                   
+    };
+  }
+}
