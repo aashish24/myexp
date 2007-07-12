2d0a6f06d9bb430f81b7a2651541da79cd5a4d56
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-07-12 16:30:27 +0000

    - Added comments

--- Indra/tests/Tweek/Veda/ITweek/TweekTest.java
+++ Indra/tests/Tweek/Veda/ITweek/TweekTest.java
@@ -1,39 +1,17 @@
-/**
- * <p>Title: </p>
- *
- * <p>Description: </p>
- *
- * <p>Copyright: Copyright (c) 2005</p>
- *
- * <p>Company: </p>
- *
- * @author Aashish Chaudhary
- * @version 1.0
- */
 
 package Veda.ITweek;
 
 import java.awt.BorderLayout;
-
-import javax.swing.JPanel;
-
-import javax.swing.*;
-
 import java.awt.*;
-
 import java.awt.event.*;
 
-import javax.swing.event.*;
-
-import org.omg.CORBA.BAD_PARAM;
-
-
-import java.awt.*;
+import javax.swing.JPanel;
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.text.*;
 
 import org.omg.CORBA.BAD_PARAM;
+
 import org.vrjuggler.tweek.event.*;
 import org.vrjuggler.tweek.net.*;
 import org.vrjuggler.tweek.net.corba.*;
@@ -41,191 +19,220 @@ import tweek.*;
 
 import Veda.*;
 
+///////////////////////////////////////////////////////////////////////////////
+//
+// Implements a simple GUI for demo purpose. 
+// 
+///////////////////////////////////////////////////////////////////////////////
+
 public class TweekTest extends JPanel implements CommunicationListener, ActionListener
 {
-    /**
-     * Sample GUI for tweek observer.
-     * Check boxes for selecting which texture to be on.
-     * Button to apply texture on the scene.
-     * Button to hide the scene.
-     */
-    private BorderLayout 	borderLayout1 = new BorderLayout(1, 4);
-    private TweekObserverImpl 	mTweekObserver = null;
     
-    //private FrameListener mFrameListener 	= null;
+  private BorderLayout 	      borderLayout1 = new BorderLayout(1, 4);
+  private TweekObserverImpl 	mTweekObserver = null;   
+
+
+  private JFrame 		          mFrame = null;
+
+  private JPanel 		          mPanel1	= null;
+  private JPanel 		          mPanel2	= null;
+
+  private JButton 		        mToggleTextureBtn = null;
+  private JButton 		        mToggleSceneBtn = null;
+
+  private int 		            mHide = 0;
+  private int 		            mTextureOn = 1;
+
+  ///////////////////////////////////////////////////////////////////////////
+  //
+  // Constructor. 
+  //
+  ///////////////////////////////////////////////////////////////////////////
+
+  public TweekTest() 
+  {
+    try 
+    {
+      jbInit();
+    } 
+    catch ( Exception exception ) 
+    {
+      exception.printStackTrace();
+    }
+  }
+
+
+  ///////////////////////////////////////////////////////////////////////////
+  //
+  // Initialize. 
+  //
+  ///////////////////////////////////////////////////////////////////////////
+
+  private void jbInit() throws Exception
+  {
+    mHide = 0;        
+    mFrame = new JFrame( "TweekDemo" );
+
+    Container cn = mFrame.getContentPane();
+    cn.setLayout(new BorderLayout(1, 4));
+
+    mPanel1 = new JPanel( new BorderLayout( 1, 4 ) );
+    mPanel1.setEnabled( true );
+
+    mPanel2 = new JPanel( new BorderLayout( 1, 4 ) );
+    mPanel2.setEnabled( true );
+
+    mToggleTextureBtn = new JButton( "Toggle Texture" );
+    mToggleTextureBtn.setSize( 100, 100 );
+    mToggleTextureBtn.addActionListener( this );
+
+    mToggleSceneBtn = new JButton( "Toggle Scene" );
+    mToggleSceneBtn.setSize( 100, 100 );
+    mToggleSceneBtn.addActionListener( this ); 
     
-    private JFrame 		mFrame = null;
+    mPanel1.add( mToggleTextureBtn, BorderLayout.SOUTH );        
+    mPanel2.add( mToggleSceneBtn );
 
-    private JPanel 		mPanel1	= null;
-    private JPanel 		mPanel2	= null;
-
-    private JButton 		mTextureButton = null;
-    private JButton 		mHideButton = null;
-
-    private int 		mHide = 0;
-    private int 		mTextureOn = 1;
-
-    JButton 			jButton1 = new JButton();
-    JComboBox 			jComboBox1 = new JComboBox();
-    JSlider 			jSlider1 = new JSlider();
+    cn.add( mPanel1, BorderLayout.NORTH );
+    cn.add( mPanel2, BorderLayout.SOUTH );
     
-    public TweekTest() 
-    {
-        try 
-	{
-            jbInit();
-        } 
-	catch (Exception exception) 
-        {
-            exception.printStackTrace();
-        }
-    }
+    mFrame.pack();
+    mFrame.setVisible(true);        
+  }
 
-    private void jbInit() throws Exception
-    {
-        //: Numeric variables.
-        mHide = 0;
 
-        //this. setLayout(borderLayout1);
-        //mPanel = new JPanel();
-        //mPanel.setEnabled(true);
-        mFrame = new JFrame("TweekDemo");
-        Container cn = mFrame.getContentPane();
-        cn.setLayout(new BorderLayout(1, 4));
-
-        mPanel1 = new JPanel(new BorderLayout(1, 4));
-        mPanel1.setEnabled(true);
-
-        mPanel2 = new JPanel(new BorderLayout(1, 4));
-        mPanel2.setEnabled(true);
-
-        mTextureButton = new JButton("Texture OFF");
-        mTextureButton.setSize(100, 100);
-        mTextureButton.addActionListener(this);
-
-        mHideButton = new JButton("Hide Scene");
-        mHideButton.setSize(100, 100);
-        mHideButton.addActionListener(this); //mPanel.add(mCheckBox1, BorderLayout.NORTH);
-        jButton1.setText("jButton1"); //mPanel.add(mCheckBox2, BorderLayout.NORTH);
-        //mPanel.add(mChkBoxButton, BorderLayout.SOUTH);
-        //mPanel.add(mButton, BorderLayout.SOUTH);
-
-        //: Panel 1
-        mPanel1.add(mTextureButton, BorderLayout.SOUTH);
-
-        //: Panel 2.
-        mPanel2.add(mHideButton);
-
-        cn.add(mPanel1, BorderLayout.NORTH);
-        cn.add(mPanel2, BorderLayout.SOUTH);
-        this.add(jButton1);
-        this.add(jComboBox1);
-        this.add(jSlider1);
-        mFrame.pack();
-        mFrame.setVisible(true);
-
-        //mFrameListener = new FrameListener(this);
-        //EventListenerRegistry.instance().registerListener(mFrameListener, TweekFrameListener.class);
-    }
-
-    public boolean frameClosing()
-    {
-        System.out.println("frameClosing()");
-        disconnect();
-        return true;
-    }
-
-    private void disconnect() {
-        if (mTweekObserver != null) {
-            mTweekObserver.detach();
-            mTweekObserver = null;
-        }
-    }
-
-    public void connectionOpened(CommunicationEvent e)
-    {
-        CorbaService corba_service = e.getCorbaService();
-        SubjectManager mgr = corba_service.getSubjectManager();
-
-        Subject subject = mgr.getSubject( "TweekSubject" );
-        TweekSubject tSubject = null;
-
-        try
-        {
-            tSubject = TweekSubjectHelper.narrow( subject );
-
-            if( tSubject != null)
-            {
-                mTweekObserver = new TweekObserverImpl( tSubject );
-                corba_service.registerObject( mTweekObserver, "TweekObserver" );
-                tSubject.attach( mTweekObserver._this() );
-            }
-        }
-        catch(BAD_PARAM narrow_ex)
-        {
-           System.out.println(" [TweekTest] Error narrowing down the subject: ");
-        }
-    }
-
-    public void connectionClosed(CommunicationEvent e)
-    {
-        if( mTweekObserver != null )
-        {
-            mTweekObserver.detach();
-            mTweekObserver = null;
-        }
-    }
-
+  ///////////////////////////////////////////////////////////////////////////
+  //
+  // Perform any operations here before exit. 
+  //
+  ///////////////////////////////////////////////////////////////////////////
+  
+  public boolean frameClosing()
+  {
+    System.out.println( "Exiting......" );
+    disconnect();
     
-	/*
-		public void frameStateChanged (TweekFrameEvent e)
-    
-		{
-      if ( e.getType() == TweekFrameEvent.FRAME_CLOSE )
+    return true;
+  }
+
+
+  ///////////////////////////////////////////////////////////////////////////
+  //
+  // Disconnect and detach from our subject. 
+  //
+  ///////////////////////////////////////////////////////////////////////////
+
+  private void disconnect() 
+  {
+    if ( mTweekObserver != null ) 
+    {
+      mTweekObserver.detach();
+      mTweekObserver = null;
+    }
+  }
+
+
+  ///////////////////////////////////////////////////////////////////////////
+  //
+  // Open a new connection. 
+  //
+  ///////////////////////////////////////////////////////////////////////////
+
+  public void connectionOpened( CommunicationEvent e )
+  {
+    TweekSubject tSubject = null;
+
+    CorbaService corba_service = e.getCorbaService();
+    SubjectManager mgr = corba_service.getSubjectManager();
+    Subject subject = mgr.getSubject( "TweekSubject" );
+
+    try
+    {
+      tSubject = TweekSubjectHelper.narrow( subject );
+
+      if( tSubject != null )
       {
-         disconnect();
+        mTweekObserver = new TweekObserverImpl( tSubject );
+        corba_service.registerObject( mTweekObserver, "TweekObserver" );
+        tSubject.attach( mTweekObserver._this() );
       }
-    
-		}
-
-	*/
-    public void actionPerformed(ActionEvent e)
-    {
-        if(e.getSource() == mHideButton)
-        {
-            if (mHide == 0)
-            {
-                mHide = 1;
-                if (mTweekObserver != null)
-                    mTweekObserver.getSubject().setCommand("Command1", "HIDE" );
-                mHideButton.setText("Show");
-            }
-            else
-            {
-                mHide = 0;
-                if (mTweekObserver != null)
-                    mTweekObserver.getSubject().setCommand("Command1", "SHOW");
-                mHideButton.setText("Hide");
-            }
-            System.out.println(mHide);
-
-        }
     }
+    catch( BAD_PARAM narrow_ex )
+    {
+      System.out.println( "Error no: Error narrowing down the subject: ") ;
+    }
+  }
+
+
+  ///////////////////////////////////////////////////////////////////////////
+  //
+  // Close existing connection. 
+  //
+  ///////////////////////////////////////////////////////////////////////////
+
+  public void connectionClosed( CommunicationEvent e )
+  {
+    if( mTweekObserver != null )
+    {
+      mTweekObserver.detach();
+      mTweekObserver = null;
+    }
+  }
+
+
+  ///////////////////////////////////////////////////////////////////////////
+  //
+  // Take action based on the source of the event. 
+  //
+  ///////////////////////////////////////////////////////////////////////////
+
+  public void actionPerformed( ActionEvent e )
+  {
+    if( e.getSource() == mToggleSceneBtn )
+    {
+      if( mHide == 0 )
+      {
+        mHide = 1;
+
+        if (mTweekObserver != null)
+        {
+          mTweekObserver.getSubject().setCommand( "Command1", "HIDE" );
+        }
+        
+        mToggleSceneBtn.setText( "Show" );
+      }
+      else
+      {
+        mHide = 0;
+        if (mTweekObserver != null)
+        {
+          mTweekObserver.getSubject().setCommand( "Command1", "SHOW" );
+        }
+        mToggleSceneBtn.setText( "Hide" );
+      }
+    }
+  }
 }
 
 
+///////////////////////////////////////////////////////////////////////////
+//
+// 
+//
+///////////////////////////////////////////////////////////////////////////
+
 class FrameListener extends TweekFrameAdapter
 {
-    public FrameListener (TweekTest bean)
-    {
-        this.mBean = bean;
-    }
+  public FrameListener ( TweekTest bean )
+  {
+      this.mBean = bean;
+  }
 
-    public boolean frameClosing(TweekFrameEvent e)
-    {
-        return true;
-    }
+  public boolean frameClosing( TweekFrameEvent e )
+  {
+      return true;
+  }
 
-    private TweekTest mBean = null;
+  private TweekTest mBean = null;
 }
 
