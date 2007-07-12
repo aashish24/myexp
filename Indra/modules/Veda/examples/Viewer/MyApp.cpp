c17cb585b4d156eb52b242c0f37429e067cdb12f
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-07-12 17:04:51 +0000

    - Updated to reflect setDevices change to setFeatures(... )

--- Indra/modules/Veda/examples/Viewer/MyApp.cpp
+++ Indra/modules/Veda/examples/Viewer/MyApp.cpp
@@ -35,7 +35,7 @@ MyApp::MyApp( vrj::Kernel* kern, int& argc, char** argv ) : OsgBase( kern, argc,
 
 void MyApp::appInit()
 {
-  setDevice( ALL, OFF );
+  setFeature( ALL, false );
 	setBackgroundColor( ( gmtl::Vec4f( 0.35, 0.35, 0.35, 1.0 ) ).getData() );		
   setNearFar( 0.1, 1000000000.0 );
   setSceneInitialPosition( osg::Vec3( 0.0, 10.0, 0.0 ) );
