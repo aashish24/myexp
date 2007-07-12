c20f1abd574c9098460f2fe6e6f26b3a74d17f21
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-07-12 00:34:05 +0000

    - Updated for new tweek communications..

---
+++ Indra/tests/Tweek/Veda/ITweek/TweekObserverImpl.java
@@ -0,0 +1,54 @@
+package Veda.ITweek;
+
+/**
+ * <p>Title: </p>
+ *
+ * <p>Description: </p>
+ *
+ * <p>Copyright: Copyright (c) 2005</p>
+ *
+ * <p>Company: </p>
+ *
+ * @author Aashish Chaudhary
+ * @version 1.0
+ */
+
+//import javax.swing.DefaultBoundedRangeModel;
+import javax.swing.JSlider;
+import tweek.*;
+
+
+public class TweekObserverImpl  extends ObserverPOA
+{
+    public TweekObserverImpl() {
+        try {
+            jbInit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public TweekObserverImpl(TweekSubject subject)
+    {
+        mTweekSubject = subject;
+    }
+
+    public TweekSubject getSubject()
+    {
+        return mTweekSubject;
+    }
+
+    public void update()
+    {
+        System.out.println( "Updated by SUBJECT" );
+    }
+
+    public void detach()
+    {
+        mTweekSubject.detach(this._this());
+    }
+
+    private TweekSubject mTweekSubject = null;
+    private void jbInit() throws Exception {
+    }
+}
