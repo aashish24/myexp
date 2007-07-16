c03ab9a1bb709d54911af1d9e13ce17a80d822fb
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-07-16 04:39:52 +0000

    - HAVE_OSG and other preprocessor symbols now moved to the project file. - Using __VPR_Version and __VRJ_Version instead of defining Veda's own version string for vrjuggler

--- Indra/modules/Veda/VedaDefines.h
+++ Indra/modules/Veda/VedaDefines.h
@@ -10,18 +10,9 @@
 #ifndef __VEDA_CORE_VEDADEFINES_H__
 #define __VEDA_CORE_VEDADEFINES_H__
 
-//@Todo: Find a better way to determine vrjuggler version. 
+#include "vrj/vrjParam.h"
 
-// If we are using OpenSceneGraph
-#define HAVE_OSG                1
-
-// Define vrjuggler major and minor version as the API has changes just a little bit 
-#define VRJUGGLER_MAJOR_VERSION 2
-
-#define VRJUGGLER_MINOR_VERSION 2
-
-// If we using KeyboardMouse for navigation. 
-#define HAVE_KEYBOARDMOUSE      0
+#include "vpr/vprParam.h"
 
 #endif // __VEDA_CORE_VEDADEFINES_H__
 
