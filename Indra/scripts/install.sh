852ed28494ef0fb6a1d538bc9b6a0791f30af5bd
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-07-20 18:51:08 +0000

    - VS put windows specific chars in the files.. just converted this file to unix format

--- Indra/scripts/install.sh
+++ Indra/scripts/install.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-#
-# All rights reserved.
-# BSD License: http://www.opensource.org/licenses/bsd-license.html
-# Created by: Aashish Chaudhary
-#
-###############################################################################
 
 if [ $# -ne 2 ] 
   then
-	  echo "Usage: $0 file dir"
+    echo "Usage: $0 file dir"
     echo "copies file to the destination directory"
 fi
 
