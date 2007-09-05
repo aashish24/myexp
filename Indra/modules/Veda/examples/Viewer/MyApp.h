4d26338fc10cbdd9197066d2748b8a24b4ace091
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-09-05 18:47:21 +0000

    - No need to specify vrjuggler libs now.. - AutoLink in juggler modules does that for us. Though there is a mismatch between dll naming convention generated by project files and lookup for those dlls.

--- Indra/modules/Veda/examples/Viewer/MyApp.h
+++ Indra/modules/Veda/examples/Viewer/MyApp.h
@@ -10,19 +10,19 @@
 #ifndef __MYAPP_H__
 #define __MYAPP_H__
 
-#if defined _MSC_VER && _DEBUG
-# pragma comment( lib, "osgd.lib" )
-# pragma comment( lib, "osgDBd.lib" )
-# pragma comment( lib, "osgUtild.lib" )
-# pragma comment( lib, "osgFXd.lib" )
-# pragma comment( lib, "osgTextd.lib" )
-# elif defined _MSC_VER
-# pragma comment( lib, "osg.lib" )
-# pragma comment( lib, "osgDB.lib" )
-# pragma comment( lib, "osgUtil.lib" )
-# pragma comment( lib, "osgFX.lib" )
-# pragma comment( lib, "osgText.lib" )  
-#endif
+//#if defined _MSC_VER && _DEBUG
+//# pragma comment( lib, "osgd.lib" )
+//# pragma comment( lib, "osgDBd.lib" )
+//# pragma comment( lib, "osgUtild.lib" )
+//# pragma comment( lib, "osgFXd.lib" )
+//# pragma comment( lib, "osgTextd.lib" )
+//# elif defined _MSC_VER
+//# pragma comment( lib, "osg.lib" )
+//# pragma comment( lib, "osgDB.lib" )
+//# pragma comment( lib, "osgUtil.lib" )
+//# pragma comment( lib, "osgFX.lib" )
+//# pragma comment( lib, "osgText.lib" )  
+//#endif
 
 #include "Veda/Core/OsgBase.h"
 
