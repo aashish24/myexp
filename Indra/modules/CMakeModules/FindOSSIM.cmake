4e839d0fca571bbed509679bc9c9d87793a6e0c9
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-10-15 17:01:31 +0000

    I think this looks good now. Atleast its more flexible and user friendly.

--- Indra/modules/CMakeModules/FindOSSIM.cmake
+++ Indra/modules/CMakeModules/FindOSSIM.cmake
@@ -1,13 +1,14 @@
 
-#------Find ossim Library----------------
-
+#------Set ossim include and library paths.
 FIND_PATH( OSSIM_INC_DIR $ENV{OSSIM_INC_DIR} )
+
 
 FIND_LIBRARY( OSSIM_LIB ossim $ENV{OSSIM_LIB_DIR} )
 
 
-#------Find ossimPlanet Library--------------
+#------Set ossimPlanet include and library paths.
 FIND_PATH( OSSIMPLANET_INC_DIR $ENV{OSSIMPLANET_INC_DIR} )
+
 
 FIND_LIBRARY( OSSIMPLANET_LIB ossimPlanet ${OSSIMPLANET_LIB_DIR} )
 
--- Indra/modules/CMakeModules/Indra.cmake
+++ Indra/modules/CMakeModules/Indra.cmake
@@ -1,17 +1,21 @@
 
+#------Add debug build preprocessors.
 IF( CMAKE_BUILD_TYPE STREQUAL "Debug" )
  ADD_DEFINITIONS( "-D_DEBUG" )
  SET( CMAKE_DEBUG_POSTFIX d )
 ENDIF( CMAKE_BUILD_TYPE STREQUAL "Debug" )
 
 
+#------Add release build preprocessors.
 IF( CMAKE_BUILD_TYPE STREQUAL "Release" )
  ADD_DEFINTIONS( "-DNDEBUG" )
 ENDIF( CMAKE_BUILD_TYPE STREQUAL "Release" )
 
 
+#------Macro to facilitate automatic linking with debug or release builds for libs build by project. 
 MACRO( LINK_INTERNAL TRGTNAME )
  FOREACH( LINKLIB ${ARGN} )
   TARGET_LINK_LIBRARIES( ${TRGTNAME} optimized "${LINKLIB}" debug "${LINKLIB}${CMAKE_DEBUG_POSTFIX}" )
  ENDFOREACH( LINKLIB )
 ENDMACRO( LINK_INTERNAL TRGTNAME )
+
