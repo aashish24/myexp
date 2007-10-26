70e3cf6139278c7329bb533b747327b5f3fbef69
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-10-26 00:12:02 +0000

    - Adding CMake suport to Veda.. not done yet

---
+++ Indra/modules/CMakeModules/FindGMTL.cmake
@@ -0,0 +1,6 @@
+
+FIND_PATH( GMTL_INC_DIR gmtl/gmtl.h 
+           $ENV{GMTL_INC_DIR}/*
+           /usr/local/include/* )
+               
+              
--- Indra/modules/CMakeModules/FindOmniORB.cmake
+++ Indra/modules/CMakeModules/FindOmniORB.cmake
@@ -2,19 +2,22 @@
 SET( OMNIORB_INC_DIR $ENV{OMNIORB_INC_DIR} )
 SET( OMNIORB_LIB_DIR $ENV{OMNIORB_LIB_DIR} )
 
-FIND_LIBRARY( OMNIORBTHREAD_LIB omnithread
-              ${OMNIORB_LIB_DIR}
-              /usr/local/lib64
-              /usr/local/lib )
+FIND_LIBRARY( OMNIORBTHREAD_LIB 
+	      NAMES omnithread
+              PATHS ${OMNIORB_LIB_DIR}
+                    /usr/local/lib64
+                    /usr/local/lib )
 
 
-FIND_LIBRARY( OMNIORBDYNAMIC_LIB omniDynamic
-              ${OMNIORB_LIB_DIR}
-              /usr/local/lib64
-              /usr/local/lib )
+FIND_LIBRARY( OMNIORBDYNAMIC_LIB 
+              NAMES omniDynamic4
+              PATHS ${OMNIORB_LIB_DIR}
+                    /usr/local/lib64
+                    /usr/local/lib )
               
 
-FIND_LIBRARY( OMNIORB_LIB omniORB
-              ${OMNIORB_LIB_DIR}
-              /usr/local/lib64
-              /usr/local/lib )
+FIND_LIBRARY( OMNIORB_LIB  
+              NAMES omniORB4
+              PATHS ${OMNIORB_LIB_DIR}
+                    /usr/local/lib64
+                    /usr/local/lib )
