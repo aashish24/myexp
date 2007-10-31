bbcd48723c3aa6059b73e23e84dc297b7f297ed7
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-10-31 06:16:13 +0000

    - added functors.. - removed unwanted files.

--- Experimental/TestMVC/TestMVC/Controller.h
+++ Experimental/TestMVC/TestMVC/Controller.h
@@ -1,62 +1,165 @@
 
-// Type of event
-// Source is mother of all the objects that are capable of generating an event. 
-class Event
+
+// This is our second approach which I like more than the one below. 
+// For wingman we are going to have 10 inputs which should be able to flip between analog and digital. 
+// 
+
+class IFunctor
 {
-  public:
-    Event( Source* src )
+  public: 
+    virtual bool call() = 0;
+};
+
+template< typename T > 
+class InputFunctor : public IFunctor
+{
+  public: 
+    InputFunctor( T* ptrObject, bool( T::*funcPtr )() ) : 
+      mPtrObject( ptrObject ), 
+      mFuncPtr( funcPtr )
     {
     }
 
-    Source* getSource()
+    virtual bool call()
     {
-      returm mSrc;
+      return ( *mPtrObject.*mFuncPtr )();     
     }
 
   private: 
-      Source* mSrc;
+    bool              ( T::*mFuncPtr )();
+    T*                mPtrObject;
 };
 
-// Still need to think about this class. Why we need this and if we do how we should design this. 
-class DigitalInputChangeEvent : public Event
+class IInput
+{
+};
+
+
+class ICommand;
+
+class DigitalInput : public IInput
+{ 
+  public:     
+    void addFunctorOnClick( IFunctor* iftor )
+    {
+      OnClick.push_back( iftor );
+    }
+
+    void execute()
+    {
+      for( size_t i = 0; i < OnClick.size(); ++i )
+      {
+        // operator ()
+        if( !( OnClick[i]->call() ) )
+        {
+          return;
+        }           
+      }
+    }
+
+
+  private: 
+    std::vector< IFunctor* > OnClick;
+    
+};
+
+
+// Command Interface. 
+class ICommand 
 {
   public: 
-    DigitalInputChangeEvent( Source* src ) : Event( src )
-    {
-    }
+    virtual bool execute() = 0;
 };
 
 
-// How we couple the event with the delegator??
-
-// Delegate the event and the source of the event to the handlers. 
-class Delegator
+// Concrete command. 
+class PrintCommand : public ICommand 
 {
   public: 
-    Delegator()
+    virtual bool execute()
     {
-    }
-    void addEventHandler( EventHandler* )
-    {
+      std::cout << "PrintCommand::execute()" << std::endl;
+      return true;
     }
 };
 
-
-// Event handler has to know the source of the event. As what we are going to do is 
-// attach a  funtion with the generator of the event so that we can get handle to that function. s 
-
-// I may have to visit the functor tutorials again. May be what we will do is each item would have atleast one delegator and 
-// one event handler that would be in the list. And that event handler would execute the function. 
-
-// Knowing the source of the event start the process of handling the event. 
-class EventHandler
+class PrintCommand2 : public ICommand 
 {
   public: 
-    EventHandler( Delegator* dtr )
+    virtual bool execute()
     {
-      dtr->addEventHandler( this );
+      std::cout << "PrintCommand2::execute()" << std::endl;
+      return true;
     }
 };
 
+
+//class VrjDigitalInput : public DigitalInput
+//{  
+//  // Contains the gaget interface which we will query later to get data.. 
+//};
+
+
+
+
+// Type of event
+// Source is mother of all the objects that are capable of generating an event. 
+//class Event
+//{
+//  public:
+//    Event( Source* src )
+//    {
+//    }
+//
+//    Source* getSource()
+//    {
+//      returm mSrc;
+//    }
+//
+//  private: 
+//      Source* mSrc;
+//};
+//
+//// Still need to think about this class. Why we need this and if we do how we should design this. 
+//class DigitalInputChangeEvent : public Event
+//{
+//  public: 
+//    DigitalInputChangeEvent( Source* src ) : Event( src )
+//    {
+//    }
+//};
+//
+//
+//// How we couple the event with the delegator??
+//
+//// Delegate the event and the source of the event to the handlers. 
+//class Delegator
+//{
+//  public: 
+//    Delegator()
+//    {
+//    }
+//    void addEventHandler( EventHandler* )
+//    {
+//    }
+//};
+//
+//
+//// Event handler has to know the source of the event. As what we are going to do is 
+//// attach a  funtion with the generator of the event so that we can get handle to that function. s 
+//
+//// I may have to visit the functor tutorials again. May be what we will do is each item would have atleast one delegator and 
+//// one event handler that would be in the list. And that event handler would execute the function. 
+//
+//// Knowing the source of the event start the process of handling the event. 
+//class EventHandler
+//{
+//  public: 
+//    EventHandler( Delegator* dtr )
+//    {
+//      dtr->addEventHandler( this );
+//    }
+//};
+//
 
 
--- Experimental/TestMVC/TestMVC/main.cpp
+++ Experimental/TestMVC/TestMVC/main.cpp
@@ -3,11 +3,86 @@
 #include "Model.h"
 #include "View.h"
 
-int main()
+#include "Controller.h"
+
+#include <GL/glut.h>
+
+DigitalInput mouseKey1;
+DigitalInput mouseKey2;
+DigitalInput mouseKey3;
+
+void
+reshape(int w, int h)
 {
-  Model* model = new Model();
+  /* Because Gil specified "screen coordinates" (presumably with an
+     upper-left origin), this short bit of code sets up the coordinate
+     system to correspond to actual window coodrinates.  This code
+     wouldn't be required if you chose a (more typical in 3D) abstract
+     coordinate system. */
 
-  View* view = new View( model );
-
-  view->render();
+  glViewport(0, 0, w, h);       /* Establish viewing area to cover entire window. */
+  glMatrixMode(GL_PROJECTION);  /* Start modifying the projection matrix. */
+  glLoadIdentity();             /* Reset project matrix. */
+  glOrtho(0, w, 0, h, -1, 1);   /* Map abstract coords directly to window coords. */
+  glScalef(1, -1, 1);           /* Invert Y axis so increasing Y goes down. */
+  glTranslatef(0, -h, 0);       /* Shift origin up to upper-left corner. */
 }
+
+void
+display(void)
+{
+  glClear(GL_COLOR_BUFFER_BIT);
+  glBegin(GL_TRIANGLES);
+    glColor3f(0.0, 0.0, 1.0);  /* blue */
+    glVertex2i(0, 0);
+    glColor3f(0.0, 1.0, 0.0);  /* green */
+    glVertex2i(200, 200);
+    glColor3f(1.0, 0.0, 0.0);  /* red */
+    glVertex2i(20, 200);
+  glEnd();
+  glFlush();  /* Single buffered, so needs a flush. */
+}
+
+
+void mouse( int button, int state, int x, int y )
+{ 
+  if( state == GLUT_DOWN )
+  {
+    if( button == GLUT_LEFT_BUTTON )
+    {
+      mouseKey1.execute();
+    }
+  }
+}
+
+int
+main(int argc, char **argv)
+{ 
+
+  PrintCommand  pCommand;
+  PrintCommand2 pCommand2;
+
+  mouseKey1.addFunctorOnClick( new InputFunctor< PrintCommand > ( &pCommand, &PrintCommand::execute ) );
+  mouseKey1.addFunctorOnClick( new InputFunctor< PrintCommand2 > ( &pCommand2, &PrintCommand2::execute ) );
+
+  mouseKey2.addFunctorOnClick( new InputFunctor< PrintCommand > ( &pCommand, &PrintCommand::execute ) );
+  mouseKey3.addFunctorOnClick( new InputFunctor< PrintCommand > ( &pCommand, &PrintCommand::execute ) );
+
+  glutInit( &argc, argv );
+  glutCreateWindow( "single triangle" );
+  glutDisplayFunc( display );
+  glutReshapeFunc( reshape );
+  glutMouseFunc( mouse );
+  glutMainLoop();
+  return 0;             /* ANSI C requires main to return int. */
+}
+
+
+//int main()
+//{
+//  Model* model = new Model();
+//
+//  View* view = new View( model );
+//
+//  view->render();
+//}
