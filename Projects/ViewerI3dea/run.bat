ad2ee7e34cc589ffef991e2a545ae0257a413eed
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-11-04 01:52:59 +0000

    - Minor change in the output message

--- Projects/ViewerI3dea/run.bat
+++ Projects/ViewerI3dea/run.bat
@@ -1,6 +1,6 @@
 @echo 
 
-@echo "Changing current directory to where the bat file exists..."
+@echo "Changing current directory to the directory where the bat file exists..."
 for /f %%i in ("%0") do set curpath=%%~dpi
 cd /d %curpath% 
 ViewerI3dea.exe %*
