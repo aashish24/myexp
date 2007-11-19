179ec085b03753793200f0e6661055a3c238999d
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-11-19 05:16:43 +0000

    - Added support for reading data from dir or file pointed by OSG_DATA_PATH..

--- Projects/ViewerI3dea/OsgViewer.cpp
+++ Projects/ViewerI3dea/OsgViewer.cpp
@@ -21,6 +21,10 @@
 
 #include <iostream>
 
+#include <boost/filesystem/operations.hpp>
+#include <boost/utility.hpp>
+#include <boost/progress.hpp>
+
 #include "GlslShaders.h"
 
 // Global list of shders programs. 
@@ -157,135 +161,217 @@ void addSceneLight( osg::Node* node )
 	root->addChild( transformLight.get() );
 }
 
+osg::Node* loadData( const std::string& file )
+{
+  osg::ref_ptr< osg::Node > node = osgDB::readNodeFile( file );
+  if( node.valid() )
+  {
+    return node.release();
+  }
+  else
+  {
+    return 0x00;
+  }
+}
+
+osg::Node* loadData( const boost::filesystem::path& path )
+{
+  if( boost::filesystem::is_directory( path ) )
+    {
+    osg::ref_ptr< osg::Group > group( new osg::Group() ); 
+
+    boost::filesystem::directory_iterator endItr;
+    boost::filesystem::directory_iterator itr( path ); 
+    
+    try
+    {
+      for( itr; itr != endItr; ++itr )
+      {
+        if( boost::filesystem::is_directory( itr->status() ) )
+        {
+          loadData( itr->path() );
+        }
+        else
+        {
+          std::cout << "Reading file: " << itr->path().string() << std::endl;
+          osg::ref_ptr< osg::Node > node = loadData( itr->path().string() );
+          if( node.valid() )
+          {
+            group->addChild( node.get() );
+          }
+        }
+      }
+    }
+    catch( const std::exception& ex )
+    {
+      std::cerr << "Exception raised: " << ex.what() << std::endl;
+    }
+
+    return group.release();
+  }
+  else
+  {
+    return loadData( path.string() );
+  }
+}
+
+// Look for data in a specific folder set by environment 
+// variable OSG_DATA_PATH
+osg::Node* loadDataEnvVarOsgDataPath()
+{
+  const char* path = getenv( "OSG_DATA_PATH" );
+  
+  if( !path )
+  {
+    std::cout << "Environment variable OSG_DATA_PATH not set: " << std::endl;
+    return 0x00;
+  }
+
+  boost::filesystem::path bPath( path, boost::filesystem::native ); 
+  if( !bPath.empty() )
+  {
+    return loadData( bPath );
+  }
+  else
+  {
+    std::cout << "Error reading data from: " << bPath.string() << std::endl;
+    return 0x00;
+  }
+}
+
 int main(int argc, char** argv)
 {
-    // use an ArgumentParser object to manage the program arguments.
-    osg::ArgumentParser arguments(&argc,argv);
+  // use an ArgumentParser object to manage the program arguments.
+  osg::ArgumentParser arguments(&argc,argv);
 
-    arguments.getApplicationUsage()->setApplicationName(arguments.getApplicationName());
-    arguments.getApplicationUsage()->setDescription(arguments.getApplicationName()+" is the standard OpenSceneGraph example which loads and visualises 3d models.");
-    arguments.getApplicationUsage()->setCommandLineUsage(arguments.getApplicationName()+" [options] filename ...");
-    arguments.getApplicationUsage()->addCommandLineOption("--image <filename>","Load an image and render it on a quad");
-    arguments.getApplicationUsage()->addCommandLineOption("--dem <filename>","Load an image/DEM and render it on a HeightField");
-    arguments.getApplicationUsage()->addCommandLineOption("-h or --help","Display command line parameters");
-    arguments.getApplicationUsage()->addCommandLineOption("--help-env","Display environmental variables available");
-    arguments.getApplicationUsage()->addCommandLineOption("--help-keys","Display keyboard & mouse bindings available");
-    arguments.getApplicationUsage()->addCommandLineOption("--help-all","Display all command line, env vars and keyboard & mouse bindings.");
-    arguments.getApplicationUsage()->addCommandLineOption("--SingleThreaded","Select SingleThreaded threading model for viewer.");
-    arguments.getApplicationUsage()->addCommandLineOption("--CullDrawThreadPerContext","Select CullDrawThreadPerContext threading model for viewer.");
-    arguments.getApplicationUsage()->addCommandLineOption("--DrawThreadPerContext","Select DrawThreadPerContext threading model for viewer.");
-    arguments.getApplicationUsage()->addCommandLineOption("--CullThreadPerCameraDrawThreadPerContext","Select CullThreadPerCameraDrawThreadPerContext threading model for viewer.");
+  arguments.getApplicationUsage()->setApplicationName(arguments.getApplicationName());
+  arguments.getApplicationUsage()->setDescription(arguments.getApplicationName()+" is the standard OpenSceneGraph example which loads and visualises 3d models.");
+  arguments.getApplicationUsage()->setCommandLineUsage(arguments.getApplicationName()+" [options] filename ...");
+  arguments.getApplicationUsage()->addCommandLineOption("--image <filename>","Load an image and render it on a quad");
+  arguments.getApplicationUsage()->addCommandLineOption("--dem <filename>","Load an image/DEM and render it on a HeightField");
+  arguments.getApplicationUsage()->addCommandLineOption("-h or --help","Display command line parameters");
+  arguments.getApplicationUsage()->addCommandLineOption("--help-env","Display environmental variables available");
+  arguments.getApplicationUsage()->addCommandLineOption("--help-keys","Display keyboard & mouse bindings available");
+  arguments.getApplicationUsage()->addCommandLineOption("--help-all","Display all command line, env vars and keyboard & mouse bindings.");
+  arguments.getApplicationUsage()->addCommandLineOption("--SingleThreaded","Select SingleThreaded threading model for viewer.");
+  arguments.getApplicationUsage()->addCommandLineOption("--CullDrawThreadPerContext","Select CullDrawThreadPerContext threading model for viewer.");
+  arguments.getApplicationUsage()->addCommandLineOption("--DrawThreadPerContext","Select DrawThreadPerContext threading model for viewer.");
+  arguments.getApplicationUsage()->addCommandLineOption("--CullThreadPerCameraDrawThreadPerContext","Select CullThreadPerCameraDrawThreadPerContext threading model for viewer.");
 
-    // if user request help write it out to cout.
-    bool helpAll = arguments.read("--help-all");
-    unsigned int helpType = ((helpAll || arguments.read("-h") || arguments.read("--help"))? osg::ApplicationUsage::COMMAND_LINE_OPTION : 0 ) |
-                            ((helpAll ||  arguments.read("--help-env"))? osg::ApplicationUsage::ENVIRONMENTAL_VARIABLE : 0 ) |
-                            ((helpAll ||  arguments.read("--help-keys"))? osg::ApplicationUsage::KEYBOARD_MOUSE_BINDING : 0 );
-    if (helpType)
-    {
-        arguments.getApplicationUsage()->write(std::cout, helpType);
-        return 1;
-    }
+  // if user request help write it out to cout.
+  bool helpAll = arguments.read("--help-all");
+  unsigned int helpType = ((helpAll || arguments.read("-h") || arguments.read("--help"))? osg::ApplicationUsage::COMMAND_LINE_OPTION : 0 ) |
+                          ((helpAll ||  arguments.read("--help-env"))? osg::ApplicationUsage::ENVIRONMENTAL_VARIABLE : 0 ) |
+                          ((helpAll ||  arguments.read("--help-keys"))? osg::ApplicationUsage::KEYBOARD_MOUSE_BINDING : 0 );
+  if (helpType)
+  {
+      arguments.getApplicationUsage()->write(std::cout, helpType);
+      return 1;
+  }
 
-    osgViewer::Viewer viewer(arguments);
+  osgViewer::Viewer viewer(arguments);
+  
+  // report any errors if they have occurred when parsing the program arguments.
+  if (arguments.errors())
+  {
+      arguments.writeErrorMessages(std::cout);
+      return 1;
+  }  
+
+  // set up the camera manipulators.
+  {
+      osg::ref_ptr<osgGA::KeySwitchMatrixManipulator> keyswitchManipulator = new osgGA::KeySwitchMatrixManipulator;
+
+      keyswitchManipulator->addMatrixManipulator( '1', "Trackball", new osgGA::TrackballManipulator() );
+      keyswitchManipulator->addMatrixManipulator( '2', "Flight", new osgGA::FlightManipulator() );
+      keyswitchManipulator->addMatrixManipulator( '3', "Drive", new osgGA::DriveManipulator() );
+      keyswitchManipulator->addMatrixManipulator( '4', "Terrain", new osgGA::TerrainManipulator() );
+
+      std::string pathfile;
+      char keyForAnimationPath = '5';
+      while (arguments.read("-p",pathfile))
+      {
+          osgGA::AnimationPathManipulator* apm = new osgGA::AnimationPathManipulator(pathfile);
+          if (apm || !apm->valid()) 
+          {
+              unsigned int num = keyswitchManipulator->getNumMatrixManipulators();
+              keyswitchManipulator->addMatrixManipulator( keyForAnimationPath, "Path", apm );
+              keyswitchManipulator->selectMatrixManipulator(num);
+              ++keyForAnimationPath;
+          }
+      }
+
+      viewer.setCameraManipulator( keyswitchManipulator.get() );
+  }
+
+  // add the state manipulator
+  viewer.addEventHandler( new osgGA::StateSetManipulator(viewer.getCamera()->getOrCreateStateSet()) );
+  
+  // add the thread model handler
+  viewer.addEventHandler(new osgViewer::ThreadingHandler);
+
+  // add the window size toggle handler
+  viewer.addEventHandler(new osgViewer::WindowSizeHandler);
+      
+  // add the stats handler
+  viewer.addEventHandler(new osgViewer::StatsHandler);
+
+  // add the help handler
+  viewer.addEventHandler(new osgViewer::HelpHandler(arguments.getApplicationUsage()));
+
+  // add the record camera path handler
+  viewer.addEventHandler(new osgViewer::RecordCameraPathHandler);
+
+  // SceneGraph creation.  
+  osg::ref_ptr< osg::Group > root ( new osg::Group() );
+
+  // load the data
+  osg::ref_ptr< osg::Node > loadedModel = osgDB::readNodeFiles(arguments);
+  if ( loadedModel.valid() ) 
+  { 
+  }
+  else
+  {
+    std::cout << "No data read from command line arguments: " << std::endl;
+    std::cout << "Loading data from OSG_DATA_PATH environment variable: " << std::endl;
+    loadedModel = loadDataEnvVarOsgDataPath();
     
-    // report any errors if they have occurred when parsing the program arguments.
-    if (arguments.errors())
+    if( !loadedModel.valid() )
     {
-        arguments.writeErrorMessages(std::cout);
-        return 1;
+      std::cerr << "Failed to load data." << std::endl;
+      std::cerr << "Please check your OSG_DATA_PATH environment variable." << std::endl;
     }
-    
-    if (arguments.argc()<=1)
-    {
-        arguments.getApplicationUsage()->write(std::cout,osg::ApplicationUsage::COMMAND_LINE_OPTION);
-        return 1;
-    }
+  }
 
-    // set up the camera manipulators.
-    {
-        osg::ref_ptr<osgGA::KeySwitchMatrixManipulator> keyswitchManipulator = new osgGA::KeySwitchMatrixManipulator;
+  // any option left unread are converted into errors to write out later.
+  arguments.reportRemainingOptionsAsUnrecognized();
 
-        keyswitchManipulator->addMatrixManipulator( '1', "Trackball", new osgGA::TrackballManipulator() );
-        keyswitchManipulator->addMatrixManipulator( '2', "Flight", new osgGA::FlightManipulator() );
-        keyswitchManipulator->addMatrixManipulator( '3', "Drive", new osgGA::DriveManipulator() );
-        keyswitchManipulator->addMatrixManipulator( '4', "Terrain", new osgGA::TerrainManipulator() );
+  // report any errors if they have occurred when parsing the program arguments.
+  if (arguments.errors())
+  {
+      arguments.writeErrorMessages(std::cout);
+      return 1;
+  }
 
-        std::string pathfile;
-        char keyForAnimationPath = '5';
-        while (arguments.read("-p",pathfile))
-        {
-            osgGA::AnimationPathManipulator* apm = new osgGA::AnimationPathManipulator(pathfile);
-            if (apm || !apm->valid()) 
-            {
-                unsigned int num = keyswitchManipulator->getNumMatrixManipulators();
-                keyswitchManipulator->addMatrixManipulator( keyForAnimationPath, "Path", apm );
-                keyswitchManipulator->selectMatrixManipulator(num);
-                ++keyForAnimationPath;
-            }
-        }
+  // Add loaded models to the scene root. 
+  root->addChild( loadedModel.get() );
 
-        viewer.setCameraManipulator( keyswitchManipulator.get() );
-    }
+  // Create a scale scene root. 
+  root = createSceneScaleByBSRatio( root.get(), 1.0 );
 
-    // add the state manipulator
-    viewer.addEventHandler( new osgGA::StateSetManipulator(viewer.getCamera()->getOrCreateStateSet()) );
-    
-    // add the thread model handler
-    viewer.addEventHandler(new osgViewer::ThreadingHandler);
+  // Add light to the scene. 
+  addSceneLight( root.get() );
 
-    // add the window size toggle handler
-    viewer.addEventHandler(new osgViewer::WindowSizeHandler);
-        
-    // add the stats handler
-    viewer.addEventHandler(new osgViewer::StatsHandler);
+  // Add shaders. 
+  shaders.push_back( new I3deaApp::GlslShaders() );
 
-    // add the help handler
-    viewer.addEventHandler(new osgViewer::HelpHandler(arguments.getApplicationUsage()));
+  // Add keyboard mouse event handler. 
+  viewer.addEventHandler( new KeyHandler( root.get() ) );
 
-    // add the record camera path handler
-    viewer.addEventHandler(new osgViewer::RecordCameraPathHandler);
-	
-	// SceneGraph creation.  
-	osg::ref_ptr< osg::Group > root ( new osg::Group() );
+  // Finally hand it off to the viewer. 
+  viewer.setSceneData( root.get() );
 
-    // load the data
-    osg::ref_ptr< osg::Node > loadedModel = osgDB::readNodeFiles(arguments);
-    if (!loadedModel) 
-    {
-        std::cout << arguments.getApplicationName() <<": No data loaded" << std::endl;
-        return 1;
-    }
+  viewer.realize();
 
-    // any option left unread are converted into errors to write out later.
-    arguments.reportRemainingOptionsAsUnrecognized();
-
-    // report any errors if they have occurred when parsing the program arguments.
-    if (arguments.errors())
-    {
-        arguments.writeErrorMessages(std::cout);
-        return 1;
-    }
-	
-	// Add loaded models to the scene root. 
-	root->addChild( loadedModel.get() );
-
-	// Create a scale scene root. 
-	root = createSceneScaleByBSRatio( root.get(), 1.0 );
-
-	// Add light to the scene. 
-	addSceneLight( root.get() );
-
-	// Add shaders. 
-	shaders.push_back( new I3deaApp::GlslShaders() );
-
-	// Add keyboard mouse event handler. 
-	viewer.addEventHandler( new KeyHandler( root.get() ) );
-
-	// Finally hand it off to the viewer. 
-    viewer.setSceneData( root.get() );
-
-    viewer.realize();
-
-    viewer.run();
+  viewer.run();
 }
