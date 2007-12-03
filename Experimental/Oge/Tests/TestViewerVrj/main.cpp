d8f5aa2e4a38d710fe92c6300f1c715c53e16b07
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2007-12-03 19:59:29 +0000

    - Project file updated to reflect changes. - Test is now setting the camera..

--- Experimental/Oge/Tests/TestViewerVrj/main.cpp
+++ Experimental/Oge/Tests/TestViewerVrj/main.cpp
@@ -15,7 +15,7 @@ int main( int argc, char **argv )
   osg::ArgumentParser arguments(&argc, argv);
 
   // load the scene.
-  osg::ref_ptr<osg::Node> loadedModel = osgDB::readNodeFiles(arguments);
+  osg::ref_ptr< osg::Node > loadedModel = osgDB::readNodeFiles( arguments );
   if (!loadedModel)
   {
       std::cout << argv[0] <<": No data loaded." << std::endl;
@@ -25,6 +25,8 @@ int main( int argc, char **argv )
   ViewerVrj* viewer = new ViewerVrj;
 
   viewer->addDataModel( loadedModel.get() );  
+
+  viewer->getCamera()->set( 0.0, -10.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0 );
 
   viewer->run();
 }
