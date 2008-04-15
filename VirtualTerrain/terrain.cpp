5ef7abcd16e08a853a55511169ac7b5db396eb46
vinay543 <vinay543@85616edc-882e-0410-a580-89c8858dbc8f>
2008-04-15 22:49:25 +0000

    - Fixed texture swapping

--- VirtualTerrain/terrain.cpp
+++ VirtualTerrain/terrain.cpp
@@ -23,6 +23,8 @@ SimpleSceneManager *mgr;
 //we have a forward declarion here, just to sort the code 
 int setupGLUT( int *argc, char *argv[] );
 
+
+
 NodePtr localModel (  UInt8* black_white_image_data_recived,  UInt32 width,  int xpoint, int zpoint ,  Pnt3f hit_point, bool event, float scale_value = 1.0);
 
 const float scale = 0.8; 
@@ -31,64 +33,40 @@ GroupNodePtr n ;
 
 Matrix geometrymatrix;
  
-UInt32  height_colour_image, width_colour_image, width, height;
+UInt32  width, height;
  
 UInt8* black_white_image_data;
  
-UInt8* colour_image_data;
- 
-//double length, breadth;
-
 std::vector<NodePtr> localRoots;
 
+GeometryPtr geo;
 
-
- void keyboard(unsigned char key, int x, int y)
- {
-        
-       if((key == 'q')||(key == 'Q'))
-	{
-	        OSG::osgExit();
-                exit(0);
-        }
-         
-         if(key == ' ')
-         {
-                
-                Pnt3f hit_point;
-                
-                Line l;
-                
-                l = mgr->calcViewRay(x, y);
-
-                IntersectAction *act = IntersectAction::create();
-                
-                act->setLine( l );
-                act->apply( n );
-             
-                if (act->didHit())
-                {
-                        hit_point = act->getHitPoint();
-                    
-                        NodePtr SourceNode = localModel( black_white_image_data, width, 0, 0, hit_point, 1);        ////calling the function //////////////
-                        localRoots.push_back(SourceNode);
-                }
-                
-              
-                beginEditCP(n, Node::ChildrenFieldMask );
-                
-                        for(int i=0; i<localRoots.size(); ++i)
-                                n.node()->addChild( localRoots[i] );
-                
-	        endEditCP(n, Node::ChildrenFieldMask );
-                
-                
-                glutPostRedisplay();     
-         }
-          
- }
+ImageRefPtr colour_image, black_white, housing_image, empty_image ;
  
- 
+/*
+void createTerrainTexture()
+{
+
+SimpleTexturedMaterialPtr tex = SimpleTexturedMaterial::create();
+	        beginEditCP(tex);
+		        tex->setImage( colour_image);
+	        endEditCP(tex);
+	        
+	        
+ SimpleTexturedMaterialPtr tex1 = SimpleTexturedMaterial::create();
+	        beginEditCP(tex1);
+		        tex1->setImage( housing_image);
+	        endEditCP(tex1);	        
+
+
+
+ SimpleTexturedMaterialPtr tex2 = SimpleTexturedMaterial::create();
+	        beginEditCP(tex2);
+		        tex2->setImage( empty_image);
+	        endEditCP(tex2);
+}
+*/
+
  
  
  NodePtr localModel (  UInt8* black_white_image_data_recived,  UInt32 width,  int xpoint, int zpoint ,  Pnt3f hit_point, bool event, float scale_value)
@@ -193,9 +171,9 @@ std::vector<NodePtr> localRoots;
  
  ////the function which is generalized which will check the case like whether any part is 
  ////a colour completly and will return a boolean value
- ////it will take the pointer of image and the width and the length and breadth of teh model 
+ ////it will take the pointer of image and the width and the length and breadth of the model 
  
- bool checkCase(  UInt32 z_length,  UInt32  x_length,   UInt32 breadth,   UInt32 length,   UInt8* empty_image_data,   UInt32 width_housing_image)
+ bool check_for_not_red(  UInt32 z_length,  UInt32  x_length,   UInt32 breadth,   UInt32 length,   UInt8* test_image_data,   UInt32 width_image)
  {
  
         bool not_adjacent= false, adjacent =false;
@@ -205,7 +183,7 @@ std::vector<NodePtr> localRoots;
 	        {
 	              
 	                                                
-	                if( empty_image_data[(width_housing_image * i  * 3) + (j  * 3) + 0] != 255)
+	                if( test_image_data[(width_image * i  * 3) + (j  * 3) + 0] != 255)
 	                {              
 	                        not_adjacent = true;                 
 	                }
@@ -225,9 +203,173 @@ std::vector<NodePtr> localRoots;
 	          break;
 	   } 
 	   
-	   return not_adjacent;          
+	   return not_adjacent;
+ 
+ 
+}
+
+
+///this function will chesk for whetehr the terrain part is blue or not and return true or false
+bool check_for_blue(  UInt32 z_length,  UInt32  x_length,   UInt32 breadth,   UInt32 length,   UInt8* test_image_data,   UInt32 width_image)
+ {
+ 
+        bool not_adjacent= false, adjacent =false;
+         for( UInt32 j = z_length; j <= (z_length + breadth); j++ ) 
+	 {
+	        for( UInt32 i = x_length ; i <= (x_length + length); i++)
+	        {
+	              
+	                                                
+	                if( test_image_data[(width_image * j  * 3) + (i  * 3) + 0] != 255)
+	                {              
+	                        not_adjacent = true;                 
+	                }
+	                                                
+	                else
+	                {
+	                        not_adjacent = false;
+	                        adjacent=true;
+	                                                        
+	                        break;
+	                }
+	                                                       
+	                                                
+	          } 
+	                                        
+	          if(adjacent)  
+	          break;
+	   } 
+	   
+	   return not_adjacent;
+	   
+} 
+ 
+
+
+ void colouring_empty_terrain(UInt32 z_length,  UInt32  x_length,   UInt32 breadth,   UInt32 length,   UInt8* empty_image_data,   UInt32 width_housing_image)
+ {
+ 
+       for( UInt32 i = x_length ; i <= (x_length + length); i++)
+        {
+                
+                 for( UInt32 j = z_length; j <= (z_length + breadth); j++ ) 
+                {
+                        //clouring that part of terrain where the model is been placed 
+                        empty_image_data[(width_housing_image * i  * 3) + (j  * 3) + 0] = 255;
+	                                                       
+                } 
+        }       
+ }
+
+
+
+
+
+////keyboard fuction to handel all teh keyboard inputs
+void keyboard(unsigned char key, int x, int y)
+ {
+        
+       if((key == 'q')||(key == 'Q'))
+	{
+	        OSG::osgExit();
+                exit(0);
+        }
+         
+         if(key == ' ')
+         {
+                
+                Pnt3f hit_point;
+                
+                Line l;
+                
+                l = mgr->calcViewRay(x, y);
+
+                IntersectAction *act = IntersectAction::create();
+                
+                act->setLine( l );
+                act->apply( n );
+             
+                if (act->didHit())
+                {
+                        hit_point = act->getHitPoint();
+                    
+                        NodePtr SourceNode = localModel( black_white_image_data, width, 0, 0, hit_point, 1);        ////calling the function //////////////
+                        localRoots.push_back(SourceNode);
+                }
+                
+              
+                beginEditCP(n, Node::ChildrenFieldMask );
+                
+                        for(int i=0; i<localRoots.size(); ++i)
+                                n.node()->addChild( localRoots[i] );
+                
+	        endEditCP(n, Node::ChildrenFieldMask );
+                
+                
+                     
+         }
+         
+         if(key == '2')
+         {      
+	       SimpleTexturedMaterialPtr tex = SimpleTexturedMaterial::create();
+	        beginEditCP(tex);
+		        tex->setImage(housing_image);
+	        endEditCP(tex);
+	        
+	       
+	        beginEditCP(geo, Geometry::MaterialFieldMask );
+
+		        geo->setMaterial( tex );
+		    
+	        endEditCP(geo,  Geometry::MaterialFieldMask );
+	        
+         }
+         
+         if(key == '1')
+         {          
+	     
+	        
+	      SimpleTexturedMaterialPtr tex = SimpleTexturedMaterial::create();
+	        beginEditCP(tex);
+		        tex->setImage( colour_image);
+	        endEditCP(tex);
+	        
+	       
+	        beginEditCP(geo, Geometry::MaterialFieldMask );
+
+		        geo->setMaterial( tex );
+		    
+	        endEditCP(geo,  Geometry::MaterialFieldMask );
+	        
+	       
+	        
+         }
+         
+         if(key == '3')
+         {               
+               
+	       SimpleTexturedMaterialPtr tex = SimpleTexturedMaterial::create();
+	        beginEditCP(tex);
+		        tex->setImage( empty_image);
+	        endEditCP(tex);
+	        
+	       
+	        beginEditCP(geo, Geometry::TexCoordsFieldMask );
+
+		        geo->setMaterial( tex );
+		    
+	        endEditCP(geo,  Geometry::TexCoordsFieldMask );
+	        
+	       
+	        
+         }
+         
+         
+       glutPostRedisplay();   
  }
  
+
+
 
 
 GroupNodePtr createScenegraph(void)
@@ -236,18 +378,18 @@ GroupNodePtr createScenegraph(void)
         ofstream outputFIle("c:vinay\\terrain_generation\\output.txt");
 
         /*********** defineing the texture image for texture mapping**********/
-        ImagePtr colour_image ;
+       
 	colour_image = Image::create();
 	colour_image -> read("colour_image.png");
 	
-	width_colour_image   =  colour_image->getWidth();
-	height_colour_image  =  colour_image->getHeight();
-	colour_image_data    =  colour_image->getData();
+	UInt32 width_colour_image   =  colour_image->getWidth();
+	UInt32 height_colour_image  =  colour_image->getHeight();
+	UInt8* colour_image_data    =  colour_image->getData();
 	
 		
 	
 	/*********** defineing the png  image for reading th evaluse from the image**********/
-	ImagePtr black_white = Image::create();
+	black_white = Image::create();
 	black_white -> read("black_white_image.pgm");	
         
 	width  =  black_white->getWidth();	
@@ -258,29 +400,20 @@ GroupNodePtr createScenegraph(void)
 	
 	/***************************taking a image which i same size of colour image************************/
 	
-	ImagePtr housing_image = Image::create();
-	//housing_image -> read("housing.png");
-	housing_image -> read("housing_128.png");
+	 housing_image = Image::create();
+	housing_image -> read("housing.png");
+	//housing_image -> read("housing_128.png");
 	UInt32 width_housing_image   =  housing_image->getWidth();
 	UInt32 height_housing_image  =  housing_image->getHeight();
 	UInt8* housing_image_data  =  housing_image->getData();
 	
-	//std::cout<< width_housing_image << std::endl;
-	//(colour_image_recived[( (width_colour_image * i * 3) + (j * 3) + 0)]) = 255
-	UInt32 blue=0;
-	for(int x=0; x<height; x++)
-		for(int z=0; z<width; z++)
-		{
+	std::cout << width_housing_image<<std::endl;
 	
-	                if(  housing_image_data[(width_housing_image * x * 3) + (z* 3) + 2] == 255)
-	                        blue++;
-	                        
-	        }
-	//std::cout<<blue<<std::endl;
+	
 	/****************************taking an empty image to use as reference***************************/
 	
-	ImagePtr empty_image ;
-	         empty_image       = Image::create();
+	
+	empty_image       = Image::create();
 	
 	beginEditCP(empty_image);
 	empty_image->set(Image::OSG_RGB_PF, width_housing_image, height_housing_image);
@@ -291,7 +424,8 @@ GroupNodePtr createScenegraph(void)
 	/**************************************************************************************************/
 
         /*****************************addding the image part ********************************************/
-	SimpleTexturedMaterialPtr tex = SimpleTexturedMaterial::create();
+	SimpleTexturedMaterialPtr tex ;
+	tex = SimpleTexturedMaterial::create();
 	beginEditCP(tex);
 		tex->setImage(colour_image);
 	endEditCP(tex);
@@ -302,7 +436,7 @@ GroupNodePtr createScenegraph(void)
 		for(int j=0; j<width; j++)
 		{
 			wHeight[i1][j] = black_white_image_data[((width*i1)+j)];
-			//std::cout<< wHeight[i1][j] << std::endl;
+			
 	        }//// looking whether a model is already there so as to check the if other nodel is placed it does not overlap
         /**************************type pointers,,,,,,quads in this case*****************************************/
 	GeoPTypesPtr type = GeoPTypesUI8::create();
@@ -344,8 +478,8 @@ GroupNodePtr createScenegraph(void)
 	endEditCP(indices, GeoIndicesUI32::GeoPropDataFieldMask);
 
         /**************************************adding all the above to the core node*****************************************/
-	GeometryPtr geo = Geometry::create();
-	beginEditCP(geo, Geometry::TypesFieldMask  |  Geometry::LengthsFieldMask | Geometry::IndicesFieldMask  |  Geometry::PositionsFieldMask | Geometry::TexCoordsFieldMask );
+        geo = Geometry::create();
+	beginEditCP(geo, Geometry::TypesFieldMask  |  Geometry::LengthsFieldMask | Geometry::IndicesFieldMask  |  Geometry::PositionsFieldMask | Geometry::TexCoordsFieldMask | Geometry::MaterialFieldMask );
 
 		geo->setTypes( type );
 		geo->setLengths( len );
@@ -355,7 +489,7 @@ GroupNodePtr createScenegraph(void)
 		geo->setMaterial( tex );
 		geo->setDlistCache( false );
                
-	endEditCP(geo, Geometry::TypesFieldMask | Geometry::LengthsFieldMask  |  Geometry::IndicesFieldMask  |  Geometry::PositionsFieldMask  | Geometry::TexCoordsFieldMask );
+	endEditCP(geo, Geometry::TypesFieldMask | Geometry::LengthsFieldMask  |  Geometry::IndicesFieldMask  |  Geometry::PositionsFieldMask  | Geometry::TexCoordsFieldMask | Geometry::MaterialFieldMask );
 
 	calcVertexNormals(geo);
 
@@ -434,12 +568,12 @@ GroupNodePtr createScenegraph(void)
 	                              
 	                              
 	                                //// looking whether a model is already there so as to check the if other nodel is placed it does not overlap any other model
-	                                bool not_red = checkCase(z_length, x_length, breadth, length, empty_image_data, width_housing_image);           
+	                                bool not_red = check_for_not_red(z_length, x_length, breadth, length, empty_image_data, width_housing_image);           
 	                                
 	                                
 	                                
 	                                ////now checking the condtion whether the are where the model has to be placed is of desired colour or not
-	                                bool blue = checkCase(z_length, x_length, breadth, length, housing_image_data, width_housing_image);
+	                                bool blue = check_for_blue(z_length, x_length, breadth, length, housing_image_data, width_housing_image);
 	                               
 	                               
 	                               
@@ -448,9 +582,9 @@ GroupNodePtr createScenegraph(void)
 	                                ///if satisfying both the conditions like whetehr the area is blue and no other model is been there so that the model to be placed does not overlap
 	                                if( (blue == true) && (not_red == true ))
 	                                {
-	                                        std::cout << "Put house at " << height_variable << "," << width_variable << std::endl;
+	                                        //std::cout << "Put house at " << height_variable << "," << width_variable << std::endl;
 	                                        ///calling the function to add the model to the desired postion on the terrain
-	                                        
+	                                      
 	                                        if(width_housing_image == 128)
 	                                        {
 	                                                 NodePtr SourceNode = localModel( black_white_image_data, width, height_variable / (float)height_housing_image * height_colour_image, 
@@ -464,22 +598,15 @@ GroupNodePtr createScenegraph(void)
 	                                        else
 	                                        {
 	                                                NodePtr SourceNode = localModel( black_white_image_data, width, height_variable , 
-	                                                                                       width_variable ,  hit_point, 0,
-	                                                                                       1.0);        ////calling the function //////////////
+	                                                                                       width_variable ,  hit_point, 0,1.0);        ////calling the function //////////////
 	                                                                                 
                                                                         localRoots.push_back(SourceNode);
                                                 }
                                                 
                                                 
-                                                for( UInt32 j = z_length; j <= (z_length + breadth); j++ ) 
-                                                {
-	                                                for( UInt32 i = x_length ; i <= (x_length + length); i++)
-	                                                {
-	                                                        //clouring that part of terrain where the model is been placed 
-	                                                        empty_image_data[(width_housing_image * i  * 3) + (j  * 3) + 0] = 255;
-	                                                       
-	                                                } 
-	                                         }
+                                                colouring_empty_terrain(z_length, x_length, breadth, length, empty_image_data, width_housing_image);
+	                                
+                                                
 	                                         
 	                                                 
 	                                }
