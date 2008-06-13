42b393f673d49da4d17a0d336284d08fc2652246
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2008-06-13 19:10:44 +0000

    Making sure that I did not forget to commit changes..

--- Experimental/Mirage/Msg/Modules/MsgCore/Node.cpp
+++ Experimental/Mirage/Msg/Modules/MsgCore/Node.cpp
@@ -2,9 +2,13 @@
 #include "MsgCore/Node.h"
 #include "MsgCore/NodeVisitor.h"
 
+#include "MsgCore/Uniform.h"
+
 using namespace Msg;
 using namespace MsgCore;
- 
+
+Uniform1i* myUniform = new Uniform1i();
+
 void Node::accept( NodeVisitor& nv )
 {
 	// Push onto node path. 
