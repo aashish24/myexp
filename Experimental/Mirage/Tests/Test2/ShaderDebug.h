8b813901caef63d121389bc230ae1f71098c72a4
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2008-06-13 19:34:20 +0000

    Test2 is temporary .. test case..

---
+++ Experimental/Mirage/Tests/Test2/ShaderDebug.h
@@ -0,0 +1,44 @@
+
+#ifndef __SHADER_DEBUG_H__
+#define __SHADER_DEBUG_H__
+
+void printShaderInfoLog( GLuint obj )
+{
+    int infologLength = 0;
+    int charsWritten  = 0;
+    char *infoLog;
+
+	  glGetShaderiv( obj, GL_INFO_LOG_LENGTH,&infologLength );
+
+    if( infologLength > 0 )
+    {
+        infoLog = ( char *)malloc( infologLength );
+        glGetShaderInfoLog( obj, infologLength, &charsWritten, infoLog );
+        
+        #ifdef _DEBUG
+          printf( "%s\n",infoLog );
+        #endif
+        free( infoLog );
+    }
+}
+
+void printProgramInfoLog( GLuint obj )
+{
+    int infologLength = 0;
+    int charsWritten  = 0;
+    char *infoLog;
+
+  	glGetProgramiv( obj, GL_INFO_LOG_LENGTH,&infologLength );
+
+    if( infologLength > 0 )
+    {
+        infoLog = ( char * )malloc( infologLength );
+        glGetProgramInfoLog( obj, infologLength, &charsWritten, infoLog );
+        #ifdef _DEBUG
+		    printf( "%s\n",infoLog );
+        #endif
+        free( infoLog );
+    }
+}
+
+#endif __SHADER_DEBUG_H__
