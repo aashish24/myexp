84f51531adcd52b1981c9f65feff226b2ceb0a2a
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2008-06-13 19:39:12 +0000

    - Added test3

---
+++ Experimental/Mirage/Tests/Test3/main.cpp
@@ -0,0 +1,65 @@
+
+#ifdef _MSC_VER 
+#include <windows.h>
+#endif // __MSC_VER
+
+#include "GL/glew.h"
+#include "GL/glut.h" 
+#include "GL/gl.h"
+
+
+#include "MsgCore/Group.h"
+#include "MsgCore/Node.h"
+#include "MsgCore/NodeVisitor.h"
+
+#include "MsgDB/FileRead.h"
+
+
+using namespace Msg; 
+
+MsgCore::SmartPtr< MsgCore::Group > _root; 
+MsgCore::SmartPtr< MsgCore::NodeVisitor > _nv;
+
+
+void init()
+{
+  _nv = new MsgCore::NodeVisitor();
+  _root = new MsgCore::Group();
+  SmartPtr< Node > node = MsgDB::FileRead::readFile( "E:\\aashish\\src\\osve\\current\\osve\\trunk\\Experimental\\Mirage\\Data\\Models\\plane.obj" );
+  
+  if( node.valid() )
+  {
+    _root->addChild( node.get() );
+  }
+}
+
+
+void display()
+{
+  _root->accept( *_nv.get() );
+
+  glutSwapBuffers();
+}
+
+
+void reshape( int width, int height )
+{
+}
+
+
+int main( int argc, char** argv )
+{
+  glutInit( &argc, argv );
+  glutInitDisplayMode( GLUT_RGB | GLUT_DOUBLE | GLUT_ALPHA );
+  glutInitWindowPosition( 0, 0 );
+  glutInitWindowSize( 800, 800 );
+  glutCreateWindow( "Test1Msg" );
+
+  init();
+
+  glutDisplayFunc( display );
+ 
+  glutReshapeFunc( reshape );
+
+  glutMainLoop();
+}
