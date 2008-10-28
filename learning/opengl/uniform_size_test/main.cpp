5a1584e6320634b9956e31632be773b3f29a7f4c
ashish_cy <ashish_cy@85616edc-882e-0410-a580-89c8858dbc8f>
2008-10-28 23:06:58 +0000

    Added queryfor max varying size for GLSL and now including glew only when we are on windows.

--- learning/opengl/uniform_size_test/main.cpp
+++ learning/opengl/uniform_size_test/main.cpp
@@ -3,7 +3,11 @@
   #include <windows.h>
 #endif 
 
-#include "GL/glew.h"
+#if defined( WIN32 ) || defined( WINDOWS )
+  #include "GL/glew.h"
+#else
+  #define glewInit void
+#endif 
 
 #include "GL/gl.h"
 #include "GL/glut.h"
@@ -14,10 +18,12 @@
 void init()
 {
   int size_uniforms[2];
+  int size_varying;
 
   // Write query function here. 
   glGetIntegerv( GL_MAX_FRAGMENT_UNIFORM_COMPONENTS_ARB, &size_uniforms[0] );
   glGetIntegerv( GL_MAX_VERTEX_UNIFORM_COMPONENTS_ARB,   &size_uniforms[1] );
+  glGetIntegerv( GL_MAX_VARYING_FLOATS, &size_varying );
 
   // Open a file and write the outout. 
   std::ofstream file( "out.txt" );
@@ -25,6 +31,7 @@ void init()
   {
     file << "Uniforms size for Vertex   shader is : " << size_uniforms[0] << std::endl;
     file << "Uniforms size for Fragment shader is : " << size_uniforms[0] << std::endl;
+    file << "Varying  size for GLSL is: " << size_varying << std::endl;
     file.close();
 
     std::exit( 0 );
@@ -36,7 +43,7 @@ void init()
   }
 }
 
-void main( int argc, char** argv )
+int main( int argc, char** argv )
 {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
@@ -46,4 +53,6 @@ void main( int argc, char** argv )
   glewInit();
 
   init();
+  
+  return 1;
 }
