ba5c2f95a5bf0f3c468dfb3599f1edd2237be0d5
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2011-07-09 14:29:43 -0400

    First pass on adding android vtk example

---
+++ vtk/vtk-android/vtk-android.cpp
@@ -0,0 +1,15 @@
+
+#include <vtkOBJReader.h>
+#include <vtkPolyData.h>
+
+vtkSmartPointer<vtkPolyData> readFile(std::string& filename)
+{
+return 0;
+}
+
+
+int main(int argc, char **argv)
+{
+  return 0;
+}
+
