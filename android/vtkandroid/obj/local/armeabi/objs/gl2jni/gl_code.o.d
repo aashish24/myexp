eaa45bc3cf4d04e272bc6dcf7fb9a01168b101ed
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2011-07-21 22:15:41 -0400

    Added opengles2 android project

---
+++ android/vtkandroid/obj/local/armeabi/objs/gl2jni/gl_code.o.d
@@ -0,0 +1,104 @@
+/home/aashish/tools/android/android-ndk-r5c/samples/hello-gl2//obj/local/armeabi/objs/gl2jni/gl_code.o: \
+ /home/aashish/tools/android/android-ndk-r5c/samples/hello-gl2//jni/gl_code.cpp \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/jni.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/android/log.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/GLES2/gl2.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/GLES2/gl2platform.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/KHR/khrplatform.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/stdint.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/sys/_types.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/machine/_types.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/GLES2/gl2ext.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/stdio.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/sys/cdefs.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/sys/types.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/linux/posix_types.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/linux/stddef.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/linux/compiler.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/asm/posix_types.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/asm/types.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/linux/types.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/machine/kernel.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/stdlib.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/string.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/malloc.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/alloca.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/strings.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/memory.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/math.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/limits.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/sys/limits.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/linux/limits.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/machine/internal_types.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/machine/limits.h \
+ /home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/sys/syslimits.h
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/jni.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/android/log.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/GLES2/gl2.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/GLES2/gl2platform.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/KHR/khrplatform.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/stdint.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/sys/_types.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/machine/_types.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/GLES2/gl2ext.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/stdio.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/sys/cdefs.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/sys/types.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/linux/posix_types.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/linux/stddef.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/linux/compiler.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/asm/posix_types.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/asm/types.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/linux/types.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/machine/kernel.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/sys/sysmacros.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/stdlib.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/string.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/malloc.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/alloca.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/strings.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/memory.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/math.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/limits.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/sys/limits.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/linux/limits.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/machine/internal_types.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/machine/limits.h:
+
+/home/aashish/tools/android/android-ndk-r5c/platforms/android-5/arch-arm/usr/include/sys/syslimits.h:
