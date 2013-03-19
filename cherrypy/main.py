033c570135dc274cbb04ba2d88c124ec7a8dd3c2
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2013-03-18 21:27:08 -0400

    Added simple cherrypy script

--- cherrypy/main.py
+++ cherrypy/main.py
@@ -1,12 +1,15 @@
 import cherrypy
+import json
+from pymongo import connection
 
 class geowebio:
 
   @cherrypy.expose
-  def allcollections(self):
+  def collections(self):
+    result = {}
+    conn = connection.MongoClient()
+    for i in conn.database_names():
+      db = eval("conn." + i)
+      result[i] = db.collection_names()
 
-
-
-
-
-
+    return json.dumps(result)
